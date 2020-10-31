ee038ad00efa8ea2c0e96a7d994d44ae3f3bec38
kylian-thezenas <60384907+kylian-thezenas@users.noreply.github.com>
2020-10-31 16:43:58 +0100

    base pour le javaFX
    J'ai créé la structure pour le javaFX puisqu'on part la dessus apparemment 
    Mais y a rien de fait pour le moment

---
+++ Reversi/src/sample/Controller.java
@@ -0,0 +1,12 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
+public class Controller {
+    public Label pion1;
+
+    public void changerCouleur(ActionEvent actionEvent) {
+        pion1.setText("bon...");
+    }
+}
