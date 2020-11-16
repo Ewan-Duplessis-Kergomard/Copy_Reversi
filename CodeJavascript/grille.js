ee33d13a3c44410aac77effff5f3fa6cd31a3d55
Ewan <ewan.duplessis-kergomard@etu.umontpellier.fr>
2020-11-16 14:39:29 +0100

    Reversi fonctionnel, toutes erreurs de logiques réglées.

--- CodeJavascript/grille.js
+++ CodeJavascript/grille.js
@@ -3,15 +3,16 @@ const grille = canvas.getContext('2d');
 const tX = canvas.getAttribute('width');
 const tY = canvas.getAttribute('height');
 var tabPions = [[0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 1, 2, 0, 0, 0],
-                [0, 0, 0, 2, 1, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0]];
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 2, 0, 0, 0],
+    [0, 0, 0, 2, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0]];
 const largeurGrille = 400;
 const c = largeurGrille/8;
+let col, row;
 
 var tabExplo = [];
 
@@ -35,19 +36,19 @@ function dessineG(){                                //fonction qui trace le plat
 function dessinePion(){
     for (let py = 0; py < 8; py++) {
         for(let px = 0; px < 8; px++) {                             // on parcoure le tableau de jeu
-            if (tabPions[px][py] === 0){
+            if (tabPions[py][px] === 0){
                 grille.fillStyle = "rgb(0,128,0)";
                 grille.beginPath();
                 grille.arc(px*50+25, py*50+25, (c/2)-8, 0, 2*Math.PI, true);
                 grille.fill();
             }
-            if(tabPions[px][py] === 1){
+            if(tabPions[py][px] === 1){
                 grille.fillStyle = "#FFFFFF";
                 grille.beginPath();
                 grille.arc(px*50+25, py*50+25, (c/2)-8, 0, 2*Math.PI, true);
                 grille.fill();
             }
-            if(tabPions[px][py] === 2){
+            if(tabPions[py][px] === 2){
                 grille.fillStyle = "#000000";
                 grille.beginPath();
                 grille.arc(px*50+25, py*50+25, (c/2)-8, 0, 2*Math.PI, true);
@@ -57,226 +58,67 @@ function dessinePion(){
     }
 }
 
-function changerCouleur(px, py, actif, autre) {
-        if (tabPions[px + 1][py] === autre) {
-            let x = px + 1;
-            while (tabPions[x][py] === autre) {
-                x++;
-            }
-            if (tabPions[x][py] === actif) {
-                x--;
-                while (x !== px) {
-                    tabPions[x][py] = actif;
-                    x--;
-                }
-            }
-        }
-        if (tabPions[px + 1][py + 1] === autre) {
-            let x = px + 1;
-            let y = py + 1;
-            while (tabPions[x][y] === autre) {
-                x++;
-                y++;
-            }
-            if (tabPions[x][y] === actif) {
-                x--;
-                y--;
-                while ((x !== px) || (y !== py)) {
-                    tabPions[x][y] = actif;
-                    x--;
-                    y--;
-                }
-            }
-        }
-        if (tabPions[px][py + 1] === autre) {
-            let y = py + 1;
-            while (tabPions[px][y] === autre) {
-                y++;
-            }
-            if (tabPions[px][y] === actif) {
-                y--;
-                while (y !== py) {
-                    tabPions[px][y] = actif;
-                    y--;
-                }
-            }
-        }
-        if (tabPions[px - 1][py + 1] === autre) {
-            let y = py + 1;
-            let x = px - 1;
-            while (tabPions[x][y] === autre) {
-                x--;
-                y++;
-            }
-            if (tabPions[x][y] === actif) {
-                x++;
-                y--;
-                while ((y !== py) || (x !== px)) {
-                    tabPions[x][y] = actif;
-                    y--;
-                    x++;
-                }
-            }
-        }
-        if (tabPions[px - 1][py] === autre) {
-            let x = px - 1;
-            while (tabPions[x][py] === autre) {
-                x--;
-            }
-            if (tabPions[x][py] === actif) {
-                x++;
-                while (x !== px) {
-                    tabPions[x][py] = actif;
-                    x++;
-                }
-            }
-        }
-        if (tabPions[px - 1][py - 1] === autre) {
-            let x = px - 1;
-            let y = py - 1;
-            while (tabPions[x][y] === autre) {
-                x--;
-                y--;
-            }
-            if (tabPions[x][y] === actif) {
-                x++;
-                y++;
-                while ((y !== py) || (x !== px)) {
-                    tabPions[x][y] = actif;
-                    x++;
-                    y++;
-                }
-            }
-        }
-        if (tabPions[px][py - 1] === autre) {
-            let y = py - 1;
-            while (tabPions[px][y] === autre) {
-                y--;
-            }
-            if (tabPions[px][y] === actif) {
-                y++;
-                while (y !== py) {
-                    tabPions[px][y] = actif;
-                    y++;
-                }
-            }
-        }
-        if (tabPions[px + 1][py - 1] === autre) {
-            let x = px - 1;
-            let y = py - 1;
-            while (tabPions[x][y] === autre) {
-                y--;
-                x++;
-            }
-            if (tabPions[x][y] === actif) {
-                y++;
-                x--;
-                while ((y !== py) || (x !== px)) {
-                    tabPions[px][y] = actif;
-                    y++;
-                }
-            }
-        }
-        dessinePion();
+
+function exploration(coox,cooy,modx,mody,actif,autre,n){
+    if((tabPions[cooy][coox]===0 || (n!=1 &&tabPions[cooy][coox]===autre) ) && (coox+modx>-1 && coox+modx<8) && (cooy+mody>-1 && cooy+mody<8) && tabPions[cooy+mody][coox+modx]===autre){
+        return exploration(coox+modx,cooy+mody,modx,mody,actif,autre,n+1);
+    }
+    if(n!=1 && (coox+modx>-1 && coox+modx<8) && (cooy+mody>-1 && cooy+mody<8) && tabPions[cooy+mody][coox+modx]===actif){
+        return true;
+    }else{return false;}
+
 }
 
-function exploration(actif, autre){
-    tabExplo = [];
-    let y, x=0;
-    let coupJouable = [];
-    for(let px = 0; px < 8; px++) {                                     // on parcoure le tableau de jeu
-        for(let py = 0; py < 8; py++) {                                 // et on examine chaque case dans
-            if(tabPions[px][py] === 0) {                                // les huit directions si la case
-                if((py+1 < 8) && (tabPions[px][py+1] === autre)) {      // est vide, bien entendu
-                    y = py + 1;
-                    while(tabPions[px][y] === autre) {                  // tant qu'il y a des pions adverses
-                        y++;
-                    }                                                   // on avance d'une case
-                    if(tabPions[px][y] === actif) {                     // pion ordi = limite
-                        coupJouable = [px,py];
-                        tabExplo.push(coupJouable);
-                    }
-                }                                                       // on renseigne la table d'évaluation
-                if((px+1 < 8) && (py+1 < 8) && (tabPions[px+1][py+1] === autre)) {
-                    y = py + 1;
-                    x = px + 1;
-                    while(tabPions[x][y] === autre) {
-                        y++;
-                        x++;
-                    }
-                    if(tabPions[x][y] === actif) {
-                        coupJouable = [px,py];
-                        tabExplo.push(coupJouable);
-                    }
-                }
-                if((px+1 < 8) && (tabPions[px+1][py] === autre)) {
-                    x = px + 1;
-                    while(tabPions[x][py] === autre) {
-                        x++;
-                    }
-                    if(tabPions[x][py] === 2) {
-                        coupJouable = [px,py];
-                        tabExplo.push(coupJouable);
-                    }
-                }
-                if((px+1 < 8) && (py-1 >= 0) && (tabPions[px+1][py-1] === autre)) {
-                    y = py - 1;
-                    x = px + 1;
-                    while(tabPions[x][y] === autre) {
-                        y--;
-                        x++;
-                    }
-                    if(tabPions[x][y] === actif) {
-                        coupJouable = [px,py];
-                        tabExplo.push(coupJouable);
-                    }
-                }
-                if((py-1 >= 0) && (tabPions[px][py-1] === autre)) {
-                    y = py - 1;
-                    while(tabPions[px][y] === autre) {
-                        y--;
-                    }
-                    if(tabPions[px][y] === actif) {
-                        coupJouable = [px,py];
-                        tabExplo.push(coupJouable);
-                    }
-                }
-                if((px-1 >= 0) && (py-1 >= 0) && (tabPions[px-1][py-1] === autre)) {
-                    y = py - 1;
-                    x = px - 1;
-                    while(tabPions[x][y] === autre) {
-                        y--;
-                        x--;
-                    }
-                    if(tabPions[x][y] === actif) {
-                        coupJouable = [px,py];
-                        tabExplo.push(coupJouable);
-                    }
-                }
-                if((px-1 >= 0) && (tabPions[px-1][py] === autre)) {
-                    x = px - 1;
-                    while(tabPions[x][py] === autre) {
-                        x--;
-                    }
-                    if(tabPions[x][py] === actif) {
-                        coupJouable = [px,py];
-                        tabExplo.push(coupJouable);
-                    }
-                }
-                if((px-1 >= 0) && (py+1 < 8) && (tabPions[px-1][py+1] === autre)) {
-                    y = py + 1;
-                    x = px - 1;
-                    while(tabPions[x][y] === autre) {
-                        y++;
-                        x--;
-                    }
-                    if(tabPions[x][y] === actif) {
-                        coupJouable = [px,py];
-                        tabExplo.push(coupJouable);
-                    }
-                }
+function mainExplo(actif,autre){
+    tabExplo=[];
+    for(let x=0;x<8;x++){
+        for(let y=0;y<8;y++){
+            var ex1=exploration(x,y,0,1,actif,autre,1);//bas
+            var ex2=exploration(x,y,1,1,actif,autre,1);//bas+droite
+            var ex3=exploration(x,y,1,0,actif,autre,1);//droite
+            var ex4=exploration(x,y,1,-1,actif,autre,1);//haut+droite
+            var ex5=exploration(x,y,0,-1,actif,autre,1);//haut
+            var ex6=exploration(x,y,-1,-1,actif,autre,1);//haut+gauche
+            var ex7=exploration(x,y,-1,0,actif,autre,1);//gauche
+            var ex8=exploration(x,y,-1,1,actif,autre,1);//bas+gauche
+            if(ex1==true||ex2==true||ex3==true||ex4==true||ex5==true||ex6==true||ex7==true||ex8==true){
+                tabExplo.push([x,y]);
             }
         }
+    }
+}
+
+function changeCoul(coox,cooy,modx,mody,actif,autre,n){
+    if((coox+modx>-1 && coox+modx<8) && (cooy+mody>-1 && cooy+mody<8) && tabPions[cooy+mody][coox+modx]===autre){
+        var temp = changeCoul(coox+modx,cooy+mody,modx,mody,actif,autre,n+1);
+        if(temp){
+            tabChang.push([coox+modx,cooy+mody]);
+            return true;
+        }
+    }
+    if(n!=1 && (coox+modx>-1 && coox+modx<8) && (cooy+mody>-1 && cooy+mody<8) && tabPions[cooy+mody][coox+modx]===actif){
+        return true;
+    }else{return false;}
+}
+
+
+function mainChangeCoul(actif,autre){
+    tabChang=[];
+    changeCoul(col,row,0,1,actif,autre,1);
+    changeCoul(col,row,1,1,actif,autre,1);
+    changeCoul(col,row,1,0,actif,autre,1);
+    changeCoul(col,row,1,-1,actif,autre,1);
+    changeCoul(col,row,0,-1,actif,autre,1);
+    changeCoul(col,row,-1,-1,actif,autre,1);
+    changeCoul(col,row,-1,0,actif,autre,1);
+    changeCoul(col,row,-1,1,actif,autre,1);
+
+    for(let i=0;i<tabChang.length;i++){
+        var x=tabChang[i][0];
+        var y=tabChang[i][1];
+        tabPions[y][x]=actif;
+        //compte autre -1
+        //compte actif +1
     }
 }
 
@@ -285,7 +127,7 @@ function getRandomInt(max) {
 }
 
 function highlight(actif, autre){
-    exploration(actif, autre);
+    mainExplo(actif, autre);
     for (let i = 0 ; i < tabExplo.length ; i++) {
         grille.fillStyle = "rgb(234,246,13)";
         grille.beginPath();
@@ -295,13 +137,13 @@ function highlight(actif, autre){
 }
 
 function tourOrdi() {
-    exploration(1,2);
+    mainExplo(1,2);
     let random = getRandomInt(tabExplo.length);
-    let x = tabExplo[random][0];
-    let y = tabExplo[random][1];
-    tabPions[x][y] = 1;
+    col = tabExplo[random][0];
+    row = tabExplo[random][1];
+    tabPions[row][col] = 1;
+    mainChangeCoul(1,2);
     dessinePion();
-    changerCouleur(x,y,1,2);
 }
 
 function game() {
@@ -315,7 +157,7 @@ highlight(2,1);
 $("#canvas").click(function(e){
     let x = e.pageX - this.offsetLeft;
     let y = e.pageY - this.offsetTop
-    let col, row;
+
     if(x < c) {
         col = 0; }
     else if(x < 2*c) {
@@ -353,15 +195,20 @@ $("#canvas").click(function(e){
         row = 7; }
     else {
         row = 8; }
-    exploration(2,1);
+    mainExplo(2,1);
     for(let i = 0; i < tabExplo.length; i++) {
         if((tabExplo[i][0] === col) && (tabExplo[i][1] === row)) {
-            tabPions[col][row] = 2;
+            tabPions[row][col] = 2;
+            mainChangeCoul(2,1);
             dessinePion();
             tourOrdi();
             highlight(2,1);
+
         }
     }
-    changerCouleur(col,row,2,1);
+    //mainChangeCoul(2,1);
     //setTimeout(fonctionAExecuter, 2000);
+
 });
+
+
